9fe3c0214525392ea9b18e30e7025eec14efa9f7
Nikita Jawali <67922187+nikitajawali09@users.noreply.github.com>
2022-10-26 12:36:55 +0530

    code

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 public static void main(String[] args){
 System.out.println(".................. Hi everyone there ...........");
-System.out.println(".............. GM all ....................");
+System.out.println(".............. GM all ................");
 }
 }
